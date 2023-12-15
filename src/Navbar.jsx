08e689c5dc2b04032441cf8889490017702c2582
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-12-15 17:25:12 +0100

    navbar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <h1 className="text-2xl text-green-500 font-bold text-white">Nameless</h1>
+    </div>
+  );
+}
+
+export default Navbar;
