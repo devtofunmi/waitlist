3f058d6b1b919744024f8002e3fe3ab391243a1d
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-12-15 17:26:08 +0100

    main section component

---
+++ src/MainSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const MainSection = () => {
+  return (
+    <div className="flex justify-center items-center flex-col text-white mt-20">
+      <h2 className="text-5xl font-bold text-center leading-[55px]">
+        Empowering Nigerian Freelancers and Entrepreneurs
+        <br /> <span className='text-green-500'>Nameless </span>Redefining Digital Collaboration
+      </h2>
+      <p className="text-center pt-2 text-lg font-semibold">
+        Connecting Talent with Opportunity Transforming the Gig Economy
+        Experience in Nigeria
+      </p>
+      <div className="flex justify-between rounded-lg border-[2px] w-[380px] h-[50px] border-white mt-5">
+        <input
+          placeholder="your email"
+          className="bg-transparent p-2"
+          type="text"
+        />
+        <button className="bg-white text-black p-1 font-bold">
+          Join Waitlist
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default MainSection;
