d464ee4e3c435b80fefec86765ca318d705c13c4
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-12-15 17:24:55 +0100

    marque component

---
+++ src/Marque.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Marquee from "react-marquee-line";
+
+const Marque = () => {
+  return (
+    <Marquee>
+        <div className='flex'>
+      <p>Programming and Tech</p>
+      <p>Website Design</p>
+      <p>Graphic Design</p>
+      <p>Logo Design</p>
+      <p>AI Services</p>
+      <p>Voice Over</p>
+      <p>Video Explainer</p>
+      <p>Photography</p>
+      <p>SEO</p>
+      <p>Social Media</p>
+      <p>Music and Audio</p>
+      <p>Business</p>
+        </div>
+    </Marquee>
+  );
+}
+
+export default Marque
