b5ca62ccf71ab527883a2c3efda786a26c1da3c2
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-12-22 16:52:46 +0100

    implement aos

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const MainSection = () => {
+   useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
     // <div className='flex'></div>
     <div className="flex justify-center items-center flex-col text-white mt-20">
-      <h2 className="md:text-5xl text-4xl font-bold text-center leading-[50px] md:leading-[55px]">
+      <h2
+        data-aos="fade-up"
+        className="md:text-5xl text-4xl font-bold text-center leading-[50px] md:leading-[55px]"
+      >
         Empowering Nigerian Freelancers and Entrepreneurs
         <br /> <span className="text-green-500">Nameless </span>Redefining
         Digital Collaboration
       </h2>
-      <p className="text-center pt-2 text-lg font-semibold">
+      <p
+        data-aos="fade-right"
+        className="text-center pt-2 text-lg font-semibold"
+      >
         Connecting Talent with Opportunity Transforming the Gig Economy
         Experience in Nigeria
       </p>
-      <div className="md:w-[400px] md:h-[150px] w-[300px] h-[200px] bg-[#282a3e] flex flex-col items-center rounded-lg mt-10">
+      <div
+        data-aos="fade-left"
+        className="md:w-[400px] md:h-[150px] w-[300px] h-[200px] bg-[#282a3e] flex flex-col items-center rounded-lg mt-10"
+      >
         <h1 className="text-2xl text-white font-bold mt-5">
           JOIN THE WAITLIST
         </h1>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Navbar() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
+     
     <div>
-      <h1 className="text-2xl text-green-500 text-green-500 font-bold">Nameless</h1>
+      <h1 data-aos="flip-left" className="text-2xl text-green-500 text-green-500 font-bold">Nameless</h1>
     </div>
   );
 }
