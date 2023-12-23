066b3771a91b09316fd079e369205a10f026927b
devtofunmi <olayiwolajesutofunmi@gmail.com>
2023-12-23 18:59:22 +0100

    ..

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -1,45 +1,101 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Success from './Success';
 
-const MainSection = () => {
+const MainSection = ({success}) => {
+   const [email, setEmail] = useState("");
+   const [error, setError] = useState("");
+   const [successMessage, setSuccessMessage] = useState("");
+   const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+
+      if (!email) {
+        setError("Enter your email");
+        setTimeout(() => {
+         setError();
+        }, 1000);
+        console.log("Enter your email");
+      } else {
+        setSuccessMessage("Email submitted successfully");
+         setTimeout(() => {
+           setSuccessMessage();
+         }, 1500);
+        console.log("Email submitted successfully");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+        setError("An error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
    useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
+
+  const handleClose = () => {
+    // Implement logic to close the success message
+    setSuccessMessage("");
+  };
+
+
   return (
-    // <div className='flex'></div>
-    <div className="flex justify-center items-center flex-col text-white mt-20">
-      <h2
-        data-aos="fade-up"
-        className="md:text-5xl text-4xl font-bold text-center leading-[50px] md:leading-[55px]"
-      >
-        Empowering Nigerian Freelancers and Entrepreneurs
-        <br /> <span className="text-green-500">Nameless </span>Redefining
-        Digital Collaboration
-      </h2>
-      <p
-        data-aos="fade-right"
-        className="text-center pt-2 text-lg font-semibold"
-      >
-        Connecting Talent with Opportunity Transforming the Gig Economy
-        Experience in Nigeria
-      </p>
-      <div
-        data-aos="fade-left"
-        className="md:w-[400px] md:h-[150px] w-[300px] h-[200px] bg-[#282a3e] flex flex-col items-center rounded-lg mt-10"
-      >
-        <h1 className="text-2xl text-white font-bold mt-5">
-          JOIN THE WAITLIST
-        </h1>
-        <div className="flex justify-between rounded-full border-[2px] w-[280px] md:w-[350px] h-[50px] border-white mt-5 bg-white">
-          <input
-            placeholder="your email"
-            className="bg-transparent p-2"
-            type="email"
-          />
-          <button className="bg-green-500 rounded-full text-white px-5 font-bold">
-            Join
-          </button>
+    <div>
+      {successMessage && (
+        <Success success={successMessage} onClose={handleClose} />
+      )}
+
+      <div className="flex justify-center items-center flex-col text-white mt-20">
+        <h2
+          data-aos="fade-up"
+          className="md:text-5xl text-4xl font-bold text-center leading-[50px] md:leading-[55px]"
+        >
+          Empowering Nigerian Freelancers and Entrepreneurs
+          <br /> <span className="text-green-500">Nameless </span>Redefining
+          Digital Collaboration
+        </h2>
+        <p
+          data-aos="fade-right"
+          className="text-center pt-2 text-lg font-semibold"
+        >
+          Connecting Talent with Opportunity Transforming the Gig Economy
+          Experience in Nigeria
+        </p>
+
+        <div
+          data-aos="fade-left"
+          className="md:w-[400px] md:h-[150px] w-[300px] h-[200px] bg-[#282a3e] flex flex-col items-center rounded-lg mt-10"
+        >
+          <h1 className="text-2xl text-white font-bold mt-5">
+            JOIN THE WAITLIST
+          </h1>
+          {error && (
+            <div className="flex justify-center  items-center text-white  md:text-[15px] text-[12px] ">
+              <div className="flex justify-between items-center text-red-500">
+                <p>{error}</p>
+              </div>
+            </div>
+          )}
+          <div className="flex justify-between rounded-full border-[2px] w-[280px] md:w-[370px] h-[50px] border-white mt-5 bg-white">
+            <input
+              placeholder="your email"
+              className="bg-transparent focus:border-transparent rounded-full p-2 w-full "
+              type="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <button
+              onClick={handleSubmit}
+              className="bg-green-500 rounded-full text-white px-5 font-bold"
+            >
+              {loading ? <p>...</p> : <p>Join</p>}
+            </button>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/Success.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Success = ({success,onClose}) => {
+  return (
+    <div className="fixed w-full top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-90 z-10">
+      <div className="md:w-[320px] w-[200px] md:h-[120px] h-[100px] rounded-lg  bg-green-500 text-white ">
+        <div className="flex justify-end p-2">
+          <button onClick={onClose}>
+            <p>close </p>
+          </button>
+        </div>
+        <div className="flex justify-center items-center  font-bold md:text-[20px] text-[12px] ">
+          <div className="text-center font-bold">
+            <p>{success}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Success
